f39016bc79a5ef1dc6a72e771aeb825124d590d5
mnaggarse <mnaggarse@gmail.com>
2025-07-19 22:13:44 +0300

    Add footer section

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="w-full py-8 text-center">
+      <p className="text-slate-600">
+        &copy; {new Date().getFullYear()} Mahmoud Elnaggar. All rights reserved.
+      </p>
+    </footer>
+  );
+}
