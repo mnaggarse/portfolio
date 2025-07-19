7675fe5c1178fce43b8e5a825db45ce4f38a6a3e
mnaggarse <mnaggarse@gmail.com>
2025-07-19 20:51:25 +0300

    Add Contact section

---
+++ components/Contact.tsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <section
+      id="projects"
+      className="flex flex-col items-center justify-center w-full gap-16 border-b py-18"
+    >
+      <h2 className="text-center text-4xl font-bold">Get In Touch</h2>
+    </section>
+  );
+}
