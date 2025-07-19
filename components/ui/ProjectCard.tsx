2e30762c91ac8d36481ca2f65d15dd4ad1a970a2
mnaggarse <mnaggarse@gmail.com>
2025-07-19 19:00:58 +0300

    Add project card

---
+++ components/ui/ProjectCard.tsx
@@ -0,0 +1,38 @@
+import Project from "@/types/project";
+import { ExternalLink } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./card";
+
+export default function ProjectCard({ project }: { project: Project }) {
+  return (
+    <Card className="w-full md:w-fit pt-0 overflow-hidden">
+      <Image
+        width="300"
+        height="300"
+        src={project.imageLink}
+        alt={project.name}
+        className="object-cover w-full"
+      />
+      <CardHeader>
+        <CardTitle>{project.name}</CardTitle>
+        <CardDescription>{project.description}</CardDescription>
+        <CardFooter className="p-0 mt-4">
+          <Link className="w-full" href={project.link} target="_blank">
+            <Button className="w-full">
+              <ExternalLink />
+              Preview
+            </Button>
+          </Link>
+        </CardFooter>
+      </CardHeader>
+    </Card>
+  );
+}
