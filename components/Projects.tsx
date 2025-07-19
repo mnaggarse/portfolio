43d76e2b6b724cc92fe6bf733bb5866ce5131b95
mnaggarse <mnaggarse@gmail.com>
2025-07-19 20:53:17 +0300

    Center text

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,8 +3,11 @@ import ProjectCard from "./ui/ProjectCard";
 
 export default function Projects() {
   return (
-    <section className="flex flex-col items-center justify-center w-full gap-16 border-b py-18">
-      <h2 className="text-4xl font-bold">My Projects</h2>
+    <section
+      id="projects"
+      className="flex flex-col items-center justify-center w-full gap-16 border-b py-18"
+    >
+      <h2 className="text-center text-4xl font-bold">My Projects</h2>
       <div className="flex justify-center items-center gap-8 flex-wrap">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
