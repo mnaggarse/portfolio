4b7569b388f23476ff4778d537e30b683bbe773b
mnaggarse <mnaggarse@gmail.com>
2025-07-30 00:20:00 +0300

    Make text in center in mobile screen

--- components/About.tsx
+++ components/About.tsx
@@ -16,11 +16,11 @@ export default function About() {
           className="rounded-lg shadow-xl"
         />
         <div className="flex flex-col gap-4 px-2">
-          <h3 className="text-xl font-bold">
+          <h3 className="text-xl font-bold text-center md:text-left">
             Hello! I&apos;m Mahmoud, a software engineer.
           </h3>
-          <p className="text-slate-600 font-medium">
-            I’m a full-stack developer who enjoys turning ideas into real,
+          <p className="text-slate-600 font-medium text-justify md:text-left">
+            I&apos;m a full-stack developer who enjoys turning ideas into real,
             useful applications. I love learning new things, solving problems,
             and improving the way I build software. I enjoy working with others,
             sharing ideas, and staying organized as a team to deliver the best
