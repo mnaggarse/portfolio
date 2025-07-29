22606181f401257982b82656fa5b4acbd6c29a42
mnaggarse <mnaggarse@gmail.com>
2025-07-29 23:49:54 +0300

    Make grid instead of flex

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,7 +8,8 @@ export default function Projects() {
       className="flex flex-col items-center justify-center w-full gap-16 border-b py-18"
     >
       <h2 className="text-center text-4xl font-bold">My Projects</h2>
-      <div className="flex justify-center items-center gap-8 flex-wrap">
+      {/* <div className="flex justify-center items-center gap-8 flex-wrap"> */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
